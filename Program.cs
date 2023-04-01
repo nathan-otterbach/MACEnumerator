57a060daf4f042c00922b071ba71bd9160f22120
Nathan O <nathan.otterbach@gmx.de>
2023-04-01 18:39:45 +0200

    updated
    Code cleanup, and speed optimizations

--- Program.cs
+++ Program.cs
@@ -1,41 +1,64 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace MACEnumerator
 {
-    class Programm
+    class Program
     {
-        static void printAllKLength(char[] set, int k)
+        static void printAllKLength(char[] set, int k, StreamWriter writer)
         {
             int n = set.Length;
-            printAllKLengthRec(set, "", n, k);
+            StringBuilder prefix = new StringBuilder(k);
+
+            for (int i = 0; i < k; i++)
+            {
+                prefix.Append(set[0]);
+            }
+
+            while (true)
+            {
+                writer.WriteLine(prefix);
+
+                int i = k - 1;
+                while (i >= 0 && prefix[i] == set[n - 1])
+                {
+                    i--;
+                }
+
+                if (i < 0)
+                {
+                    break;
+                }
+
+                prefix[i] = set[Array.IndexOf(set, prefix[i]) + 1];
+
+                for (int j = i + 1; j < k; j++)
+                {
+                    prefix[j] = set[0];
+                }
+            }
         }
 
-        static void printAllKLengthRec(char[] set,String prefix,int n, int k)
-        {
-            if (k == 0)
-            {
-                Console.WriteLine(prefix);
-                return;
-            }
-            for (int i = 0; i < n; ++i)
-            {
-                String newPrefix = prefix + set[i];
-                printAllKLengthRec(set, newPrefix,n, k - 1);
-            }
-        }
-
-        static public void Main()
+        static void Main()
         {
             char[] set1 = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-            int k = 2;
+            int k;
 
-            FileStream filestream = new FileStream("mac.txt", FileMode.Create);
-            var streamwriter = new StreamWriter(filestream);
-            streamwriter.AutoFlush = true;
-            Console.SetOut(streamwriter);
-            Console.SetError(streamwriter);
-            
-            printAllKLength(set1, k);
+            while (true)
+            {
+                Console.Write("Enter the value of k (between 1 and {0}): ", set1.Length);
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out k) && k >= 1 && k <= set1.Length)
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a number between 1 and {0}", set1.Length);
+            }
+            using (var writer = new StreamWriter("mac.txt", false, Encoding.ASCII, 65536)) // set the buffer size to 64KB
+            {
+                printAllKLength(set1, k, writer);
+            }
         }
     }
 }
